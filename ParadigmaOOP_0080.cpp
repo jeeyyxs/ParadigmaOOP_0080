f39cb479f5d97734fd4ae9b35de6d6da36d604a4
jeeyyxs <ajengrahmadhani02@gmail.com>
2026-04-21 14:55:58 +0700

    data 4

--- ParadigmaOOP_0080.cpp
+++ ParadigmaOOP_0080.cpp
@@ -8,5 +8,7 @@ class Mahasiswa{
         float nilai;
         void printData(){
             cout << "NIM = "<< nim <<endl;
+            cout << "Nama = "<< nama <<endl;
+            cout << "Nilai = "<< nilai <<endl;
         }
 }
